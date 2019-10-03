c2de8cd22a393f821fb2210fcf65d1b09e408602
jedrzejewskiandrzej <jederk3027@gmail.com>
2019-10-03 08:51:05 +0200

    first

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SmAnSm</title>
+  </head>
+  <link rel="stylesheet" href="style.css">
+  <body>
+    <header>
+      Login
+    </header>
+
+    <content>
+      <form class="" method="post">
+        <input type="text" class="register" placeholder="Login"><br><br>
+        <input type="text" class="register" placeholder="Password"><br><br>
+        <input type="text" class="register" placeholder="Email"><br><br>
+        <input type="button" id="but1" value="Next">
+      </form>
+    </content>
+    <footer>
+
+    </footer>
+  </body>
+</html>
