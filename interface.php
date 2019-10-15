48557932d1da168105291665f38170d56c7c24c0
jedrzejewskiandrzej <jederk3027@gmail.com>
2019-10-15 19:31:10 +0200

    Interfejs usera + css: faza 1

---
+++ interface.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Interfejs</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style media="screen">
+      <?php
+        include './css/widescreen.css';
+       ?>
+    </style>
+  </head>
+  <body>
+
+        <menu class="col-1-1 inteMenu">
+          aa
+        </menu>
+
+        <nav class="col-9 inteNav">
+          <ul>
+            <li>Powiadomienia</li>
+            <li>Napisz wiadomość</li>
+            <li>Mój profil</li>
+            <li>Zmień hasło</li>
+            <li>Wyloguj</li>
+          </ul>
+        </nav>
+
+        <main class="col-9 inteMain">
+
+        </main>
+
+
+
+
+
+
+  </body>
+</html>
