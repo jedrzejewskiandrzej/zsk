354ffe10782685a1ef7c77b14d38ec3fb3386243
jedrzejewskiandrzej <jederk3027@gmail.com>
2019-10-28 21:31:02 +0100

    Poprawiona część z sekcjami i css... Do zrobienia zawartość sections

--- interface.php
+++ interface.php
@@ -13,20 +13,63 @@
   <body>
 
         <menu class="col-1-1 inteMenu">
-          aa
+          <img src="./images/img.jpg" alt="Supeł logo">
+          <ul class="col-12">
+            <li><a href="#">Strona główna</a></li>
+            <li><a href="#">Moje zestawy</a></li>
+            <li><a href="#">Kategorie</a></li>
+            <li><a href="#">Szybka powtórka</a></li>
+          </ul>
         </menu>
 
         <nav class="col-9 inteNav">
           <ul>
-            <li>Powiadomienia</li>
-            <li>Napisz wiadomość</li>
-            <li>Mój profil</li>
-            <li>Zmień hasło</li>
-            <li>Wyloguj</li>
+            <li><a href="#">Powiadomienia</a></li>
+            <li><a href="#">Napisz wiadomość</a></li>
+            <li><a href="#">Mój profil</a></li>
+            <li><a href="#">Zmień hasło</a></li>
+            <li><a href="#">Wyloguj</a></li>
           </ul>
         </nav>
 
         <main class="col-9 inteMain">
+
+          <section class="col-12 sec1">
+
+            <div class="col-3-1">
+
+            </div>
+
+            <div class="col-3-1">
+
+            </div>
+
+            <div class="col-3-1">
+
+            </div>
+
+            <div class="col-3-1">
+
+            </div>
+
+          </section>
+
+          <section class="col-12 sec2">
+
+            <div class=" fleft">
+
+            </div>
+
+            <div class=" fright">
+
+            </div>
+
+          </section>
+
+          <section class="col-12 sec3">
+            <div class="col-12">
+            </div>
+          </section>
 
         </main>
 
