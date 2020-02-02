926e1d5065e114c482ee838065bc9a8c2117d866
jedrzejewskiandrzej <jederk3027@gmail.com>
2020-02-02 21:34:49 +0100

    Walidacja rejestracji

--- register.php
+++ register.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,16 +8,93 @@
     <title>SmAnSm</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style media="screen">
-      <?php
-        include './css/widescreen.css';
-       ?>
-    </style>
+    
+    <link rel="stylesheet" href="./css/widescreen.css">
   </head>
   <body>
 
     <?php
     require_once("./scripts/connect.php");
+
+    $name = $lastname = $login = $email = $email = $type = $password = $errorName = $errorLastname = $errorLogin = $errorEmail = $errorType = $errorPassword = '';
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      if (empty($_POST["name"])) {
+        $errorName = "Podaj imię.";
+        } else {
+          $name = test_input($_POST["name"]);
+          if (!preg_match("/^[A-Z][a-z]{2,}$/",$name)) {
+            $errorName = "Upewnij się, że zacząłeś wielką literą i użyłeś tylko liter.";
+        }else {
+          $_SESSION["name"] = test_input($_POST["name"]);
+        }
+      }
+
+      if (empty($_POST["lastname"])) {
+        $errorLastname = "Podaj nazwisko.";
+        } else {
+          $lastname = test_input($_POST["lastname"]);
+          if (!preg_match("/^[A-Z][a-z]{2,}$/",$lastname)) {
+            $errorLastname = "Upewnij się, że zacząłeś wielką literą i użyłeś tylko liter.";
+          }else {
+            $_SESSION["lastname"] = test_input($_POST["lastname"]);
+          }
+      }
+
+      if (empty($_POST["login"])) {
+        $errorLogin = "Podaj login.";
+      } else {
+        $login = test_input($_POST["login"]);
+        if (!preg_match("/^[0-9a-zA-Z]{4,}$/",$login)) {
+          $errorLogin = "Minimum 4 znaki";
+        }else{
+          $_SESSION["login"]  = test_input($_POST["login"]);
+        }
+      }
+
+      if (empty($_POST["email"])) {
+        $errorEmail = "Podaj email.";
+      } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $errorEmail = "Niepoprawny email";
+        }else{
+          $_SESSION["email"]  = test_input($_POST["email"]);
+        }
+      }
+
+      if (empty($_POST["type"])) {
+        $errorType = "Gender is required";
+      } else {
+        $type = test_input($_POST["type"]);
+        $_SESSION["type"] = test_input($_POST["type"]);
+      }
+
+      if (empty($_POST["password"])) {
+        $errorPassword = "Podaj hasło.";
+      } else {
+        $password = test_input($_POST["password"]);
+        if (!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[\!\@\#\$\%\^\&\*\(\)\_\+\-\=])(?=.*[A-Z])(?!.*\s).{8,}$/",$password)) {
+          $errorPassword = "Niepoprawne hasło. Musi zawierać małe i wielkie litery, minmum 1 znak specjalny, minium 1 cyfrę oraz co najmniej 8 znaków";
+        }else{
+          $_SESSION["password"] = test_input($_POST["password"]);
+        }
+      }
+
+  if(!empty($_SESSION['password'])){ //wystarczy sprawdzić jedno pole w związku z tym, że cały formularz jest wymagany...akurat wybrałem sonbie hasło
+  header('location: ./scripts/add_user.php');
+  }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
      ?>
 
     <content>
@@ -24,17 +104,25 @@
 
     </aside>
 
-    <form class="register" method="post" action="./scripts/add_user.php">
-      <input type="text" name="name" placeholder="Podaj imię" autofocus><br><br>
-      <input type="text" name="lastname" placeholder="Podaj nazwisko"><br><br>
-      <input type="text" name="login" placeholder="Podaj login" required><br><br>
-      <input type="text" name="email" placeholder="Podaj email" required><br><br>
+    <form class="register" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+      <input type="text" name="name" maxlength="20" placeholder="Podaj imię" value="<?php echo $name;?>" autofocus>
+      <?php if($errorName!= null){?> <span class="red_label"><?php echo $errorName; ?> </span> <?php } ?><br><br>
+      <input type="text" name="lastname" maxlength="30" placeholder="Podaj nazwisko" value="<?php echo $lastname;?>">
+      <?php if($errorLastname!= null){?> <span class="red_label"><?php echo $errorLastname; ?> </span> <?php } ?><br><br>
+      <input type="text" name="login" maxlength="20" placeholder="Podaj login" value="<?php echo $login;?>">
+      <?php if($errorLogin!= null){?> <span class="red_label"><?php echo $errorLogin; ?> </span> <?php } ?><br><br>
+      <input type="text" name="email" maxlength="30" placeholder="Podaj email" value="<?php echo $email;?>">
+      <?php if($errorEmail!= null){?> <span class="red_label"><?php echo $errorEmail; ?> </span> <?php } ?><br><br>
       <input type="radio" name="type" value="s" checked>Uczen
       <input type="radio" name="type" value="t">Nauczyciel
       <input type="radio" name="type" value="a">Admin<br><br>
-      <input type="password" name="password" placeholder="Podaj hasło" required><br><br>
+
+      <input type="password" name="password" maxlength="30" placeholder="Podaj hasło">
+      <?php if($errorPassword!= null){?> <span class="red_label"><?php echo $errorPassword; ?> </span> <?php } ?><br><br>
       <input type="submit" name="btn1" value="Zarejestruj">
-    </form>
+
+
 
       </div>
     </content>
--- scripts/add_user.php
+++ scripts/add_user.php
@@ -1,20 +1,19 @@
 <?php
+session_start();
 require_once("./connect.php");
 
-if(isset($_POST['btn1']) && isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['login']) && isset($_POST['email']) && isset($_POST['type']) && isset($_POST['password'])){
-  $name = $_POST['name'];
-  $lastname = $_POST['lastname'];
-  $login = $_POST['login'];
-  $email = $_POST['email'];
-  $type = $_POST['type'];
-  $password = $_POST['password'];
-  echo $name." ".$lastname." ".$login." ".$email." ".$type." ".$password;
+$name = $_SESSION["name"];
+$lastname = $_SESSION["lastname"];
+$login = $_SESSION["login"];
+$email = $_SESSION["email"];
+$type = $_SESSION["type"];
+$password = $_SESSION["password"];
 
   $sql = "INSERT INTO `user`(`name`, `lastname`, `login`, `email`, `type`, `password`) VALUES ('$name','$lastname','$login','$email','$type','$password')";
   if(mysqli_query($connect, $sql)){
-    //echo "Poprawnie dodano usera";
     header('location: ../signin.php');
     }
-}
+
 mysqli_close($connect);
+session_destroy();
  ?>
